c0005e6bc21ebe6edecdcd288bd68a9d465e264e
Collaborateur1 <gennade.jv@gmail.com>
2016-08-30 17:26:44 -0600

    update for Test with public access

--- src/main/java/controller/PublicAccess.java
+++ src/main/java/controller/PublicAccess.java
@@ -1,20 +1,34 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
 import com.sun.jersey.api.view.Viewable;
 
+import model.bean.DefaultCustomer;
+import model.custom.CustomerCustom;
+import model.custom.DossierCustom;
+import model.custom.EnterpriseCustom;
+import model.custom.UserCustom;
+import model.job.GenericJob;
+import other.SpringFactory;
 import view.handlebars.HandlebarsManager;
 
 
@@ -54,4 +68,117 @@ private Handlebars publicTemplate=HandlebarsManager.get();
             return Response.ok(new Viewable("/WEB-INF/htmlFile/login.html")).cookie( new NewCookie( "cookie",randomUUIDString ) ).build();
         }
     }
+    
+    @javax.annotation.security.PermitAll
+    @GET
+    @Path("/test")
+    @Produces( MediaType.APPLICATION_JSON )
+    public Response get() throws JsonProcessingException {
+      
+      
+        GenericJob test= SpringFactory.getGenericJob();
+        voidAdd100Person();
+        voidAdd100Person();
+        voidAdd200Dossier();
+         
+        voidAdd100Enterprise();
+        DossierCustom dossier=(DossierCustom) test.getObject( DossierCustom.class,"1",false);
+       
+        //test.loadLazyCollection( user );
+        /*
+       List<String> result= test.getList( UserCustom.class, new String[][]{{"userDossier","dossier"}},new String[][]{{"dossier.dossierHistory","=","ae97d5d9-cafe-45bc-83c7-51c74dd501f3"}}, null, new String[][]{{"PROPERTY","email"},{"PROPERTY","nom"},{"PROPERTY","prenom"},{"PROPERTY","dossier.dossierHistory"}},10000,-1);
+       // List<String> result= test.getList( UserCustom.class,null,new String[][]{{"nom","like","magkilapuls"}}, null, null);
+       return Response.ok().entity( result).build();
+      
+      
+        voidAdd100Person();
+        voidAdd100Person();
+        voidAdd200Dossier();
+         
+        voidAdd100Enterprise();
+          
+        */
+        
+        return Response.ok().entity( dossier).build();
+    }
+    
+    
+    public void voidAdd100Person(){
+       
+        GenericJob test= SpringFactory.getGenericJob();
+        for (int i=0;i<10;i++){
+            UserCustom user=new UserCustom();
+            user.setDusName( UUID.randomUUID().toString() );
+            user.setDusSurname( UUID.randomUUID().toString() );
+            user.setDusAddress( UUID.randomUUID().toString() );
+            user.setDusEmail(UUID.randomUUID().toString()+"@msn.com");
+            user.setDusMdp( "123456789".getBytes().toString() );
+            user.setDusNumber( UUID.randomUUID().toString());
+            user.setDusToken( UUID.randomUUID().toString() );
+            test.createObject( user );
+            
+        }
+        
+    }
+    
+    public void voidAdd200Dossier(){
+        
+        
+        GenericJob test= SpringFactory.getGenericJob();
+        List<HashMap> result= test.getList( UserCustom.class, null,null, null, new String[][]{{"PROPERTY","id"}},20,-1);
+        for (int i=0;i<result.size();i++){
+            String id="";
+             id=result.get( i ).get( "id" ).toString();
+             
+            
+            DossierCustom dossier=new DossierCustom();
+            dossier.setDosCreation( new Date() );
+            UserCustom testt=(UserCustom)test.getObject( UserCustom.class, id ,false);
+           
+            dossier.setDosCreator( testt);
+            dossier.setDosHistory( UUID.randomUUID().toString() );
+            test.createObject( dossier );
+            ArrayList arr= new ArrayList();
+            arr.add( dossier );
+            testt.setDusDossier( arr );
+            test.updateObject(testt);
+            
+        }
+        
+    }
+    
+   public void voidAdd100Enterprise(){
+        
+        
+       GenericJob test= SpringFactory.getGenericJob();
+       for (int i=0;i<100;i++){
+           EnterpriseCustom ent=new EnterpriseCustom();
+           ent.setEntBirth( new Date() );
+           ent.setEntName( UUID.randomUUID().toString() );
+           ent.setEntUpdate(new Date() );
+      
+           
+           CustomerCustom cust=new CustomerCustom();
+           cust.setCusBith( new Date() );
+           cust.setCusCivility( DefaultCustomer.civility.Mr.toString());
+           cust.setCusFirstName( UUID.randomUUID().toString() );
+           cust.setCusMail( UUID.randomUUID().toString() );
+           cust.setCusNumber( UUID.randomUUID().toString() );
+           ArrayList arr= new ArrayList();
+           arr.add( cust );
+        
+           ent.setEntCustomers( arr );
+           test.createObject(cust ); 
+           test.createObject(ent );
+           
+     
+           
+       }
+       
+      
+            
+        }
+        
+       
+    
 }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,31 +1,34 @@
-
+import model.job.GenericJob;
+import other.SpringFactory;
 
 public class test {
 
     public static void main( String[] args ) {
         // TODO Auto-generated method stub
        
-   
-long time,time2;
+        GenericJob test= SpringFactory.getGenericJob();
+/*long time,time2;
 time=System.currentTimeMillis();
-int c=1000000;
+int c=1000;
 
-String[][] st= new String[5][2];
-
-System.out.println(st.length );
-//String st="";
-//
-//for(int i=0;i<c;i++)
+String[][] st=new String[][]{{"1","2","3"},{"1","2","3"}};
+System.out.println(st[0][0]);
+//System.out.println(st.length );
+String titi="qzdqzsdqzsdqszdqsd:qsdqsdqsdc:qzdcqsd:qsdqs:dqsd:qsdqsd:qszdqsz:dqsdc:qsd:qsd:qsdqs:dqs:dqs:dqs:dqsd:qsdqsdqsd:qefdqsfcdsfdsf:sdfsdfdsfds:dsfdsfdsfdsf:dsfdsfdsfds:sdgfdfdsfds:dsfdsgdsgdsfsdfds:dsgfdsfsdefdsf:dsfdsfsdfds:sfdesfdsfdsfdsf";
+String[] toto=titi.split( ":" );
+//for(int i=0;i<st.length;i++)
 //{
-//    st=st+"azertyuiopqsdfghjklmwxcvbn1234567899874563210+-*";
+//    st[i][0]="azertyuiopqsdfghjklmwxcvbn"+i;
+//    st[i][1]="wxcvbnqsdfghjklmpoiuytreza"+i;
 //    
 //}
-
-//StringBuilder st=new StringBuilder();
+*/
+//StringBuilder st2=new StringBuilder(2500);
 //
 //for(int i=0;i<c;i++)
 //{
-//    st.append("azertyuiopqsdfghjklmwxcvbn1234567899874563210+-*");
+//    st2.append("azertyuiopqsdfghjklmwxcvbn:"+i);
+//    st2.append("wxcvbnqsdfghjklmpoiuytreza"+i);
 //    
 //}
 
@@ -37,10 +40,10 @@ System.out.println(st.length );
 //    st.append("azertyuiopqsdfghjklmwxcvbn1234567899874563210+-*");
 //    
 //}
-
+/*
 time2=System.currentTimeMillis();
 System.out.println( (time2-time)+" Milli" );
-System.out.println( ((time2-time)/1000)+" Seconde" );
+System.out.println( ((time2-time)/1000)+" Seconde" );*/
 }
 
 }
