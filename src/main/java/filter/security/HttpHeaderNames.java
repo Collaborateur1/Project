f198eb33fc52f43ca39277f8a37002cd8c380843
Collaborateur1 <gennade.jv@gmail.com>
2016-10-03 12:12:36 -0600

    headerName for response filter

---
+++ src/main/java/filter/security/HttpHeaderNames.java
@@ -0,0 +1,7 @@
+package filter.security;
+
+public interface HttpHeaderNames {
+    public static final String SERVICE_KEY = "service_key";
+    public static final String AUTH_TOKEN = "auth_token";
+   
+}
